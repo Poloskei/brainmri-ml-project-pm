95871fddcd47b886f5595f7f5688fa124db2929a
Palcsek Máté <73887796+Poloskei@users.noreply.github.com>
2023-12-13 20:38:06 +0000

    asfda

--- Hello.py
+++ Hello.py
@@ -64,6 +64,7 @@ if __name__ == "__main__":
 
     kerasmodel = load_models()
     img = __load_and_preprocess_custom_image()
-    st.write("image uploaded")
-    st.image(img)
-    st.write("keras: " +__predict_score(img,kerasmodel))
+    if img is not null:
+      st.write("image uploaded")
+      st.image(img)
+      st.write("keras: " +__predict_score(img,kerasmodel))
